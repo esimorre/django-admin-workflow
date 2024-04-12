3f09bc5d943b8ce58226e59f838dbda9006cdbf8
SIMORRE Eric <eric.simorre@csgroup.eu>
2024-04-12 10:52:45 +0200

    remove workflow module

--- django_admin_workflow/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class DjangoAdminWorkflowConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_admin_workflow'
-    verbose_name = 'Configuration workflow'
